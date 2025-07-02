78330dd638971ebb74e950dbc5a8906db480e542
jameselite <soroushdeveloper20@gmail.com>
2025-07-02 17:14:28 +0330

    added: support for dictionary and list

--- pyshade/interface.py
+++ pyshade/interface.py
@@ -18,8 +18,8 @@ class Pyshade:
         
         except Exception as err:
             print(err)
-            return
-        
+            return 
+            
     @staticmethod
     def encrypt_jsonkey(data: str):
 
@@ -35,6 +35,40 @@ class Pyshade:
             print(err)
             return
         
+    @staticmethod
+    def encrypt_struct(data: dict | list):
+    
+        try:
+
+            struct: str = json.dumps(data)
+            encrypted_data: str | None = Encryption.encrypt_data(struct, CharMap.char_map)
+
+            return {
+                "data": encrypted_data,
+                "key": CharMap.char_map
+            }           
+        
+        except Exception as err:
+            print(err)
+            return
+
+    @staticmethod
+    def encrypt_struct_jsonkey(data: dict | list):
+    
+        try:
+
+            struct: str = json.dumps(data)
+            encrypted_data: str | None = Encryption.encrypt_data(struct, CharMap.char_map)
+
+            return {
+                "data": encrypted_data,
+                "key": json.dumps(CharMap.char_map)
+            }           
+        
+        except Exception as err:
+            print(err)
+            return   
+
     @staticmethod
     def decrypt(data: str, secret_key: dict[str, int]) -> str | None:
 
@@ -56,6 +90,32 @@ class Pyshade:
             decrypted_data: str | None = Decryption.decryption(data, json.loads(secret_key))
 
             return decrypted_data
+        
+        except Exception as err:
+            print(err)
+            return
+        
+    @staticmethod
+    def decrypt_struct(data: str, secret_key: dict[str, int]) -> str | None:
+
+        try:
+
+            decrypted_data: str | None = Decryption.decryption(data, secret_key)
+
+            return json.loads(decrypted_data)
+        
+        except Exception as err:
+            print(err)
+            return
+        
+    @staticmethod
+    def decrypt_struct_jsonkey(data: str, secret_key: str) -> str | None:
+
+        try:
+
+            decrypted_data: str | None = Decryption.decryption(data, json.loads(secret_key))
+
+            return json.loads(decrypted_data)
         
         except Exception as err:
             print(err)
